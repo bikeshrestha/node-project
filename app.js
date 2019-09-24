274c4804be79c88e0904c820e404fc57703b158d
srpbsm@gmail.com <srpbsm@gmail.com>
2019-09-24 16:54:36 +0545

    static development

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world for the tenth time, travis ci pushing image to dockerhub succefull. Now testig with docker-compose file. Now tesing for basant jee. checking for commit hash. a4f7710\n');
+  res.send('Hello world for the tenth time, travis ci pushing image to dockerhub succefull. Now testig with docker-compose file. Now tesing for basant jee. checking for static value development change phase 1.\n');
 });
 
 app.listen(PORT, HOST);
