784b8c6d61e8dd2cad7ed2080cca15331e12a60a
srpbsm@gmail.com <srpbsm@gmail.com>
2019-09-24 17:40:59 +0545

    testing continue

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world for the tenth time, travis ci pushing image to dockerhub succefull. Now testig with docker-compose file. Now tesing for basant jee. checking for static value development change phase 1.\n');
+  res.send({message:'testing continue',
+  port:JSON.stringify(PORT),
+  testing_env:JSON.stringify(process.env.TAG)
+});
 });
 
 app.listen(PORT, HOST);
