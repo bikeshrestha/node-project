426b9c766eaac1983c46a92b89127da5713c6f29
srpbsm@gmail.com <srpbsm@gmail.com>
2019-09-25 13:44:47 +0545

    not woring why

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send({message:'image in ennv',
+  res.send({message:'why not working in ennv',
   port:JSON.stringify(PORT),
   testing_env:JSON.stringify(process.env.TAG)
 });
