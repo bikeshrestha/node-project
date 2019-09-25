037387c3cc9396f250ee641bbc216cf0b40a2281
srpbsm@gmail.com <srpbsm@gmail.com>
2019-09-25 15:53:58 +0545

    node env

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send({message:'why not working in ennv',
+  res.send({message:'node env testing',
   port:JSON.stringify(PORT),
   testing_env:JSON.stringify(process.env.TAG)
 });
