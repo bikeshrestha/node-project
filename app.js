93e50e82cbaf31ceac50ecedac3af95a4078ec7c
srpbsm@gmail.com <srpbsm@gmail.com>
2019-09-26 14:57:05 +0545

    ato

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send({message:'node env testing',
+  res.send({message:'travis auto testign',
   port:JSON.stringify(PORT),
   testing_env:JSON.stringify(process.env.TAG)
 });
